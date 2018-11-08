28cd17d79e8a47c784d34d00280a141628d05635
Michael Sheehan <sheehanpmichael@gmail.com>
2018-11-08 14:15:09 +0000

    redux working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import rootReducer from './reducers/root_reducer'
+// import React from 'react';
+// import ReactDOM from 'react-dom'
+// import { createStore } from 'redux'
+// import { Provider } from 'react-redux'
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Provider } from "react-redux";
+import { createStore } from 'redux';
+
+import rootReducer from './reducers/root_reducer';
 import App from './App';
 
 import './index.css';
@@ -14,7 +21,12 @@ const store = createStore(rootReducer);
 
 
 
-ReactDOM.render(<Provider store={store} > <App /></Provider >, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store} >
+        <App />
+    </Provider >,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
