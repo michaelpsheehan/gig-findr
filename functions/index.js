93a65c24dfd8d1a326187fdd019730c1f4fec995
Michael Sheehan <sheehanpmichael@gmail.com>
2018-11-11 18:01:33 +0000

    before showing live notifications

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
-
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
@@ -7,4 +8,36 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello G dudes");
 });
 
+const createNotification = (notification => {
+    return admin.firestore().collection('notificaions')
+        .add(notification)
+        .then(doc => console.log('notification added', doc))
+})
 
+
+exports.projectCreated = functions.firestore
+    .document('projects/{projectId}')
+    .onCreate(doc => {
+        const project = doc.data();
+        const notification = {
+            content: 'Added a new project',
+            user: `${project.authorFirstName} ${project.authorLastName}`,
+            time: admin.firestore.FieldValue.serverTimestamp()
+        }
+        return createNotification(notification);
+    })
+
+exports.userJoined = functions.auth.user()
+    .onCreate(user => {
+        return admin.firestore().collection('users')
+            .doc(user.uid).get().then(doc => {
+
+                const newUser = doc.data();
+                const notification = {
+                    content: 'joined the site',
+                    user: `${newUser.firstName[0]} ${newUser.lastName}`,
+                    time: admin.firestore.FieldValue.serverTimestamp()
+                }
+                return createNotification(notification)
+            })
+    })
